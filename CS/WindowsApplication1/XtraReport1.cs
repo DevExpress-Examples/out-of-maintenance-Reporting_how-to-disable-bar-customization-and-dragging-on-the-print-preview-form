757c712e0b8e34270500e674bd3412cbba8ff00e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WindowsApplication1/XtraReport1.cs
@@ -0,0 +1,9 @@
+
+namespace WindowsApplication1 {
+    public partial class XtraReport1 : DevExpress.XtraReports.UI.XtraReport {
+        public XtraReport1() {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to disable bar customization and dragging on the Print Preview form
+
+
+<p>This example illustrates how to disable the bar customization feature and prevent bars from dragging.</p>
+
+<br/>
+
+
